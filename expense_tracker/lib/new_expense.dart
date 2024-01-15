cc8f9a03a9c21aec317620e35b9f7dceef7c6c3e
marijoyapa <marijoy.apa@tooltwist.com>
2024-01-15 13:48:45 +0800

    create expense list item

---
+++ expense_tracker/lib/new_expense.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatelessWidget {
+  const NewExpense({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: SingleChildScrollView(child: Text('Form page')),
+    );
+  }
+}
