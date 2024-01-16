b6e3bebfb560bad7a7c266710821cb1472b3f0ce
marijoyapa <marijoy.apa@tooltwist.com>
2024-01-16 15:30:52 +0800

    create new expense form

--- expense_tracker/lib/new_expense.dart
+++ expense_tracker/lib/new_expense.dart
@@ -1,8 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/model/expense.dart';
 
-class NewExpense extends StatelessWidget {
-  const NewExpense({super.key});
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  void _submitExpenseData() {
+    //double.parse will return the double type of an object, it will return null for invalid input
+    final enteredAmount = double.tryParse(_amountController.text);
+
+    //will check if inputed amount is valid. entered should not be null or less than or equal to 0 (negative values)
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+
+    //when one of the field is invalid or empty, an AlerDialog will display with its message
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text(
+              'Please make sure a valid title, amount, date and category was entered'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
+            )
+          ],
+        ),
+      );
+      //will not proceed to execute onAddExpense function when form is invalid
+      return;
+    }
+
+    //if the form is valid, onAddExpense is called and should pass the details of the expense
+    widget.onAddExpense(Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory));
+
+    //if the addition of new expense is successful, will close the NewExpense page
+    Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,18 +86,28 @@ class NewExpense extends StatelessWidget {
       height: double.infinity,
       child: SingleChildScrollView(
         child: Padding(
-            padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
           child: Column(
             children: [
-              const TextField(
-                decoration: InputDecoration(label: Text('Title')),
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  label: Text('Title'),
+                ),
+                maxLength: 50,
+              ),
+              const SizedBox(
+                width: 16,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Expanded(
+                  Expanded(
                     child: TextField(
+                      controller: _amountController,
                       keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         prefixText: '\$',
                         label: Text('Amount'),
                       ),
@@ -33,9 +119,11 @@ class NewExpense extends StatelessWidget {
                   Expanded(
                     child: Row(
                       children: [
-                        const Text('No date selected'),
+                        Text(_selectedDate == null
+                            ? 'No date selected'
+                            : formatter.format(_selectedDate!)),
                         IconButton(
-                          onPressed: () {},
+                          onPressed: _presentDatePicker,
                           icon: const Icon(Icons.calendar_month),
                         ),
                       ],
@@ -49,7 +137,7 @@ class NewExpense extends StatelessWidget {
               Row(
                 children: [
                   DropdownButton(
-                      value: Category.travel,
+                      value: _selectedCategory,
                       items: Category.values
                           .map(
                             (category) => DropdownMenuItem(
@@ -58,14 +146,23 @@ class NewExpense extends StatelessWidget {
                             ),
                           )
                           .toList(),
-                      onChanged: (value) {}),
+                      onChanged: (value) {
+                        if (value == null) {
+                          return;
+                        }
+                        setState(() {
+                          _selectedCategory = value;
+                        });
+                      }),
                   const Spacer(),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                     child: const Text('Cancel'),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: _submitExpenseData,
                     child: const Text('Save Expense'),
                   )
                 ],
